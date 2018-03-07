658ad23aee13febdac68508cae5486ffec0ef8e5
Vinícius Queiroz Costa <viniciusqueirozcosta@outlook.com>
2018-03-07 13:29:32 -0300

    Add files via upload
    Calculadora com método construtor e Message box de erro.

---
+++ contasMatematicas_comConstructor-mensagemDeErro/src/Modelo/Controle.java
@@ -0,0 +1,93 @@
+
+package Modelo;
+
+
+public class Controle
+{
+    private String num1,num2,op,finalResult;
+    private String errorMessage;
+    
+    public Controle(String num1, String num2, String op)
+    {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.op = op;
+        calcular();
+    }
+
+    public Controle(String num1, String num2)
+    {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    
+    public void calcular()
+    {
+
+        Double n1, n2, result;
+        
+        try
+        {
+            this.errorMessage = "";
+            
+            n1 = Double.parseDouble(this.num1);
+            n2 = Double.parseDouble(this.num2);
+
+            if (this.op.equals("+"))
+            {
+                result = n1 + n2;
+                this.finalResult = result.toString();
+            }
+
+            if (this.op.equals("-"))
+            {
+                result = n1 - n2;
+                this.finalResult = result.toString();
+            }
+
+            if (this.op.equals("*"))
+            {
+                result = n1 * n2;
+                this.finalResult = result.toString();
+            }
+
+            if (this.op.equals("/"))
+            {       
+                if(n2 == 0)
+                    this.errorMessage = "Divisão por zero!";
+
+                else
+                {
+                result = n1 / n2;
+                this.finalResult = result.toString();
+                }
+            }
+        }
+        
+        catch(Exception e)
+        {
+            this.errorMessage = "Erro!";
+        }
+        
+    }
+    
+    //setters
+    public String setOp(String op)
+    {
+        return this.op = op;
+    }
+    //getter
+    public String getFinalResult()
+    {
+        return finalResult;
+    }
+
+    public String getErrorMessage()
+    {
+        return errorMessage;
+    }
+    
+    
+    
+    
+}
